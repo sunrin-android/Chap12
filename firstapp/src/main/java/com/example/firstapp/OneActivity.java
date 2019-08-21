baa9f968669a1d342c2896eb24c4c3f15f60defa
heptacode <admin@hyunwoo.kim>
2019-08-21 09:16:59 +0900

    Create secondapp

--- firstapp/src/main/java/com/example/firstapp/OneActivity.java
+++ firstapp/src/main/java/com/example/firstapp/OneActivity.java
@@ -1,7 +1,11 @@
 package com.example.firstapp;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OneActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class OneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                ComponentName componentName = new ComponentName("com.example.secondapp","com.example.secondapp.TwoActivity");
+                intent.setComponent(componentName);
+                startActivity(intent);
+            }
+        });
     }
 }
--- secondapp/src/main/java/com/example/secondapp/TwoActivity.java
+++ secondapp/src/main/java/com/example/secondapp/TwoActivity.java
@@ -1,7 +1,10 @@
 package com.example.secondapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TwoActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class TwoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TwoActivity.this, TwoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
